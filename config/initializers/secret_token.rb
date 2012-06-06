355894f6f2636b42c72e7d178b28a3eda070c852
Tejas Dinkar <tejas@gja.in>
2012-06-06 22:56:55 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jestful::Application.config.secret_token = 'bf258199c6a5fef4ee0857f7ea2e49db121f6f1afbc3d45e877b7de2eef4558228ea583e8fdc105f642c258098d3760a2f78ab62838876c88921d1cb057af30f'
